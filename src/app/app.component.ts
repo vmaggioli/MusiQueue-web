2cfc8346bc64ffa1afdbc97ddc372565c8d8b002
Vincent Maggioli <vmaggiol@purdue.edu>
2017-09-13 19:41:59 -0400

    Initialized firebase with angularfire2

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './shared/auth.service';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import * as firebase from 'firebase/app';
+
+@Component({
+  selector: 'lsl-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  topics: FirebaseListObservable<any[]>;
+  title = 'MusiQueue-web';
+  user = null;
+
+  constructor(
+    private auth: AuthService,
+    public db: AngularFireDatabase) { }
+
+  ngOnInit() {
+    this.auth.getAuthState().subscribe(
+      (user) => this.user = user);
+      this.topics = this.db.list('/topics');
+  }
+
+  loginWithGoogle() {
+    this.auth.loginWithGoogle();
+  }
+}

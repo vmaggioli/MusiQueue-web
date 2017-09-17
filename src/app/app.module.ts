365aa956ca6cabd5e6373bab83aa5de2cf669188
root <root@LAPTOP-8C23E3OF.localdomain>
2017-09-16 20:04:32 -0400

    Everything is working now
    fixed the issue with authentication and compiler
    you can click the login with google button at the bottom and the login page will come up
    added bootstrap
    added some more node stuff so this pull might take longer than usual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-//import { AngularFireModule } from 'angularfire2';
-//import { AngularFireDatabaseModule } from 'angularfire2/database';
-//import { AngularFireAuthModule } from 'angularfire2/auth';
-//import { AuthService } from './shared/auth.service';
-//import { environment } from '../environments/environment'
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AuthService } from './shared/auth.service';
+import { environment } from '../environments/environment';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAgvdKA3CBSgY3Oaooi8TDh4gA0iQgg5Ok',
+  authDomain: 'musiqueue-web.firebaseapp.com',
+  databaseURL: 'https://musiqueue-web.firebaseio.com',
+  projectID: 'musiqueue-web',
+  storageBucket: 'musiqueue-web.appspot.com',
+  messagingSenderID: '386642263895'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-//    AngularFireModule.initializeApp(environment.firebase, 'MusiQueue-web'),
-//    AngularFireDatabaseModule,
-//    AngularFireAuthModule
+    AngularFireModule.initializeApp(environment.firebase, 'MusiQueue-web'),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
-//  providers: [AuthService],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

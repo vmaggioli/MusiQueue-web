439a9fc71aa6ed3bc2abc429261c7ea5ad959f50
Vincent Maggioli <vmaggiol@purdue.edu>
2017-09-19 11:31:11 -0400

    Created basic create/join hubs page

---
+++ src/app/create-hub/create-hub.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'create-hub',
+  encapsulation: ViewEncapsulation.None,
+  templateUrl: './create-hub.component.html',
+  styleUrls: ['./create-hub.component.css']
+})
+
+export class CreateHubComponent() {
+  
+  constructor() {
+    
+  }
+  
+  ngOnInit() {
+    
+  }
+}
---
+++ src/app/join-hub/join-hub.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'join-hub',
+  encapsulation: ViewEncapsulation.None,
+  templateUrl: './join-hub.component.html',
+  styleUrls: ['./join-hub.component.css']
+})
+
+export class JoinHubComponent() {
+  
+  constructor() {
+    
+  }
+  
+  ngOnInit() {
+    
+  }
+}

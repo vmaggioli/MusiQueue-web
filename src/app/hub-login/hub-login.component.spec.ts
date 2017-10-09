0e6741b0ba845a86a459452abe98d5e8c3dcbcfb
Vincent Maggioli <vmaggiol@purdue.edu>
2017-10-08 20:11:08 -0400

    Fixed another test case

--- src/app/hub-login/hub-login.component.spec.ts
+++ src/app/hub-login/hub-login.component.spec.ts
@@ -1,14 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HubService } from '../shared/hub.service';
 import { HubLoginComponent } from './hub-login.component';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+import { AuthService } from '../shared/auth.service';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { UsersService } from '../shared/users.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HubLoginComponent', () => {
   let component: HubLoginComponent;
   let fixture: ComponentFixture<HubLoginComponent>;
+  const config = {
+    apiKey: "AIzaSyAgvdKA3CBSgY3Oaooi8TDh4gA0iQgg5Ok",
+    authDomain: "musiqueue-web.firebaseapp.com",
+    databaseURL: "https://musiqueue-web.firebaseio.com",
+    projectId: "musiqueue-web",
+    storageBucket: "musiqueue-web.appspot.com",
+    messagingSenderId: "386642263895"
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HubLoginComponent ]
+      imports: [
+        AngularFireDatabaseModule,
+        AngularFireAuthModule,
+        AngularFireModule.initializeApp(config),
+        RouterTestingModule
+       ],
+      declarations: [ HubLoginComponent ],
+      providers: [
+        HubService,
+        AuthService,
+        UsersService
+       ]
     })
     .compileComponents();
   }));
